5430eade392ff05c427877be1c6fa804dd8f45f3
Jhonny Menarim <jhonny-cwb@hotmail.com>
2018-10-22 23:59:13 -0300

    Alteração do Post

--- resources/views/partials/header/seo.blade.php
+++ resources/views/partials/header/seo.blade.php
@@ -1,5 +1,5 @@
 <meta name="description" itemprop="description" content="{{ $description or 'Proteca - Proteção da Criança e do Adolescente na Era Digital' }}">
-<meta name="keywords" itemprop="keywords" content="Proteca Proteção Criança Adolescente na Era Digital">
+<meta name="keywords" itemprop="keywords" content="projeto, proteca, proteção, criança, adolescente, era, digital, aliciamento, abuso, infantil, artigos, pesquisas, livros">
 <meta property="og:url" content="{{ env('APP_URL') }}">
 <meta property="og:title" content="{{ $title or env('APP_NAME') }}">
 <meta property="og:site_name" content="{{ env('APP_NAME') }}">
